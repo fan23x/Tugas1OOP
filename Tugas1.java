4bfe2865f686d398a3baaf44e10fc42ccf7b741a
fan23x <41988461+fan23x@users.noreply.github.com>
2019-03-07 15:29:37 +0700

    Create Tugas1.java

---
+++ Tugas1.java
@@ -0,0 +1,6 @@
+public class BIrfan {
+    public static void main(String[] args) {
+        System.out.println("NAMA    : Muhammad Irfan Bakhtiar \nNIM     : 17090047" \nKELAS   : 4B");
+    }
+    
+}
